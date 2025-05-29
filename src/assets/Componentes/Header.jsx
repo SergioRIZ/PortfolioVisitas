708a4f6ed39d6972eabb57eaf3924105b0e8ccef
Pedro <pedrocorreas92@gmail.com>
2025-05-29 09:53:07 +0200

    Header mejorado

--- src/assets/Componentes/Header.jsx
+++ src/assets/Componentes/Header.jsx
@@ -4,7 +4,12 @@ const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="main-title">Visitas Virtuales 360º</h1>
+        <h1 className="main-title">
+  <span className="typing" data-text="Visitas Virtuales 360º">
+    Visitas Virtuales 360º
+  </span>
+</h1>
+
         <div className="header-buttons">
         </div>
       </div>
